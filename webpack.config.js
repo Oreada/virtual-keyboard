a32d54ed9e612a170f909b4ea5323e94e218e6cf
Olga Antushevich <antushevich@gmail.com>
2022-05-05 19:47:47 +0300

    init: create npm webpack environment

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let conf = {
+	entry: './src/js/index.js',
+
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].[hash].js',
+		clean: true,
+	},
+
+	devServer: {
+		static: './dist',
+	},
+
+	optimization: {
+		runtimeChunk: 'single',
+	},
+
+	mode: 'development',
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'RSS Webpack',
+			filename: 'main.html',
+			template: 'src/index.html',
+		})
+	],
+
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					"style-loader",
+					// Translates CSS into CommonJS
+					"css-loader",
+					// Compiles Sass to CSS
+					"sass-loader",
+				],
+			},
+		]
+	}
+};
+
+module.exports = conf;
