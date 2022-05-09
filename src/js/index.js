143de104cde04f4b11e1647a0eeac505a953f97d
Olga Antushevich <antushevich@gmail.com>
2022-05-09 11:58:06 +0300

    fix: fix Shift action

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../css/style.css';
+// import '../css/style.css';
 import '../scss/style.scss';
 
 import { createElement } from './createElement';
@@ -7,167 +7,197 @@ const keyboardList = document.querySelector('.keyboard__list');
 
 const keysAll = [
   {
-    content: '`', code: 'Backquote', className: 'key_light key_twin', contentShift: '~', contentRu: 'ё', contentShiftRu: 'Ё',
+    content: '`', code: 'Backquote', className: 'key_light key_twin', contentShift: '~', classNameRu: 'key_light', contentRu: 'ё', contentShiftRu: 'Ё',
   },
   {
-    content: '1', code: 'Digit1', className: 'key_light key_twin', contentShift: '!', contentRu: '1', contentShiftRu: '!',
+    content: '1', code: 'Digit1', className: 'key_light key_twin', contentShift: '!', classNameRu: 'key_light key_twin', contentRu: '1', contentShiftRu: '!',
   },
   {
-    content: '2', code: 'Digit2', className: 'key_light key_twin', contentShift: '@', contentRu: '2', contentShiftRu: '"',
+    content: '2', code: 'Digit2', className: 'key_light key_twin', contentShift: '@', classNameRu: 'key_light key_twin', contentRu: '2', contentShiftRu: '"',
   },
   {
-    content: '3', code: 'Digit3', className: 'key_light key_twin', contentShift: '#', contentRu: '3', contentShiftRu: '№',
+    content: '3', code: 'Digit3', className: 'key_light key_twin', contentShift: '#', classNameRu: 'key_light key_twin', contentRu: '3', contentShiftRu: '№',
   },
   {
-    content: '4', code: 'Digit4', className: 'key_light key_twin', contentShift: '$', contentRu: '4', contentShiftRu: ';',
+    content: '4', code: 'Digit4', className: 'key_light key_twin', contentShift: '$', classNameRu: 'key_light key_twin', contentRu: '4', contentShiftRu: ';',
   },
   {
-    content: '5', code: 'Digit5', className: 'key_light key_twin', contentShift: '%', contentRu: '5', contentShiftRu: '%',
+    content: '5', code: 'Digit5', className: 'key_light key_twin', contentShift: '%', classNameRu: 'key_light key_twin', contentRu: '5', contentShiftRu: '%',
   },
   {
-    content: '6', code: 'Digit6', className: 'key_light key_twin', contentShift: '^', contentRu: '6', contentShiftRu: ':',
+    content: '6', code: 'Digit6', className: 'key_light key_twin', contentShift: '^', classNameRu: 'key_light key_twin', contentRu: '6', contentShiftRu: ':',
   },
   {
-    content: '7', code: 'Digit7', className: 'key_light key_twin', contentShift: '&', contentRu: '7', contentShiftRu: '?',
+    content: '7', code: 'Digit7', className: 'key_light key_twin', contentShift: '&', classNameRu: 'key_light key_twin', contentRu: '7', contentShiftRu: '?',
   },
   {
-    content: '8', code: 'Digit8', className: 'key_light key_twin', contentShift: '*', contentRu: '8', contentShiftRu: '*',
+    content: '8', code: 'Digit8', className: 'key_light key_twin', contentShift: '*', classNameRu: 'key_light key_twin', contentRu: '8', contentShiftRu: '*',
   },
   {
-    content: '9', code: 'Digit9', className: 'key_light key_twin', contentShift: '(', contentRu: '9', contentShiftRu: '(',
+    content: '9', code: 'Digit9', className: 'key_light key_twin', contentShift: '(', classNameRu: 'key_light key_twin', contentRu: '9', contentShiftRu: '(',
   },
   {
-    content: '0', code: 'Digit0', className: 'key_light key_twin', contentShift: ')', contentRu: '0', contentShiftRu: ')',
+    content: '0', code: 'Digit0', className: 'key_light key_twin', contentShift: ')', classNameRu: 'key_light key_twin', contentRu: '0', contentShiftRu: ')',
   },
   {
-    content: '-', code: 'Minus', className: 'key_light key_twin', contentShift: '_', contentRu: '-', contentShiftRu: '_',
+    content: '-', code: 'Minus', className: 'key_light key_twin', contentShift: '_', classNameRu: 'key_light key_twin', contentRu: '-', contentShiftRu: '_',
   },
   {
-    content: '=', code: 'Equal', className: 'key_light key_twin', contentShift: '+', contentRu: '=', contentShiftRu: '+',
-  },
-  { content: 'Backspace', code: 'Backspace', className: 'key_dark key_wide' },
-  { content: 'Tab', code: 'Tab', className: 'key_dark' },
-  {
-    content: 'q', code: 'KeyQ', className: 'key_light', contentShift: 'Q', contentRu: 'й', contentShiftRu: 'Й',
+    content: '=', code: 'Equal', className: 'key_light key_twin', contentShift: '+', classNameRu: 'key_light key_twin', contentRu: '=', contentShiftRu: '+',
   },
   {
-    content: 'w', code: 'KeyW', className: 'key_light', contentShift: 'W', contentRu: 'ц', contentShiftRu: 'Ц',
+    content: 'Backspace', code: 'Backspace', className: 'key_dark key_wide', classNameRu: 'key_dark key_wide',
   },
   {
-    content: 'e', code: 'KeyE', className: 'key_light', contentShift: 'E', contentRu: 'у', contentShiftRu: 'У',
+    content: 'Tab', code: 'Tab', className: 'key_dark', classNameRu: 'key_dark',
   },
   {
-    content: 'r', code: 'KeyR', className: 'key_light', contentShift: 'R', contentRu: 'к', contentShiftRu: 'К',
+    content: 'q', code: 'KeyQ', className: 'key_light', contentShift: 'Q', classNameRu: 'key_light', contentRu: 'й', contentShiftRu: 'Й',
   },
   {
-    content: 't', code: 'KeyT', className: 'key_light', contentShift: 'T', contentRu: 'е', contentShiftRu: 'Е',
+    content: 'w', code: 'KeyW', className: 'key_light', contentShift: 'W', classNameRu: 'key_light', contentRu: 'ц', contentShiftRu: 'Ц',
   },
   {
-    content: 'y', code: 'KeyY', className: 'key_light', contentShift: 'Y', contentRu: 'н', contentShiftRu: 'Н',
+    content: 'e', code: 'KeyE', className: 'key_light', contentShift: 'E', classNameRu: 'key_light', contentRu: 'у', contentShiftRu: 'У',
   },
   {
-    content: 'u', code: 'KeyU', className: 'key_light', contentShift: 'U', contentRu: 'г', contentShiftRu: 'Г',
+    content: 'r', code: 'KeyR', className: 'key_light', contentShift: 'R', classNameRu: 'key_light', contentRu: 'к', contentShiftRu: 'К',
   },
   {
-    content: 'i', code: 'KeyI', className: 'key_light', contentShift: 'I', contentRu: 'ш', contentShiftRu: 'Ш',
+    content: 't', code: 'KeyT', className: 'key_light', contentShift: 'T', classNameRu: 'key_light', contentRu: 'е', contentShiftRu: 'Е',
   },
   {
-    content: 'o', code: 'KeyO', className: 'key_light', contentShift: 'O', contentRu: 'щ', contentShiftRu: 'Щ',
+    content: 'y', code: 'KeyY', className: 'key_light', contentShift: 'Y', classNameRu: 'key_light', contentRu: 'н', contentShiftRu: 'Н',
   },
   {
-    content: 'p', code: 'KeyP', className: 'key_light', contentShift: 'P', contentRu: 'з', contentShiftRu: 'З',
+    content: 'u', code: 'KeyU', className: 'key_light', contentShift: 'U', classNameRu: 'key_light', contentRu: 'г', contentShiftRu: 'Г',
   },
   {
-    content: '[', code: 'BracketLeft', className: 'key_light key_twin', contentShift: '{', contentRu: 'х', contentShiftRu: 'Х',
+    content: 'i', code: 'KeyI', className: 'key_light', contentShift: 'I', classNameRu: 'key_light', contentRu: 'ш', contentShiftRu: 'Ш',
   },
   {
-    content: ']', code: 'BracketRight', className: 'key_light key_twin', contentShift: '}', contentRu: 'ъ', contentShiftRu: 'Ъ',
-  },
-  { content: 'DEL', code: 'Delete', className: 'key_dark key_wide' },
-  { content: 'CapsLock', code: 'CapsLock', className: 'key_dark key_wide' },
-  {
-    content: 'a', code: 'KeyA', className: 'key_light', contentShift: 'A', contentRu: 'ф', contentShiftRu: 'Ф',
+    content: 'o', code: 'KeyO', className: 'key_light', contentShift: 'O', classNameRu: 'key_light', contentRu: 'щ', contentShiftRu: 'Щ',
   },
   {
-    content: 's', code: 'KeyS', className: 'key_light', contentShift: 'S', contentRu: 'ы', contentShiftRu: 'Ы',
+    content: 'p', code: 'KeyP', className: 'key_light', contentShift: 'P', classNameRu: 'key_light', contentRu: 'з', contentShiftRu: 'З',
   },
   {
-    content: 'd', code: 'KeyD', className: 'key_light', contentShift: 'D', contentRu: 'в', contentShiftRu: 'В',
+    content: '[', code: 'BracketLeft', className: 'key_light key_twin', contentShift: '{', classNameRu: 'key_light', contentRu: 'х', contentShiftRu: 'Х',
   },
   {
-    content: 'f', code: 'KeyF', className: 'key_light', contentShift: 'F', contentRu: 'а', contentShiftRu: 'А',
+    content: ']', code: 'BracketRight', className: 'key_light key_twin', contentShift: '}', classNameRu: 'key_light', contentRu: 'ъ', contentShiftRu: 'Ъ',
   },
   {
-    content: 'g', code: 'KeyG', className: 'key_light', contentShift: 'G', contentRu: 'п', contentShiftRu: 'П',
+    content: 'DEL', code: 'Delete', className: 'key_dark key_wide', classNameRu: 'key_dark key_wide',
   },
   {
-    content: 'h', code: 'KeyH', className: 'key_light', contentShift: 'H', contentRu: 'р', contentShiftRu: 'Р',
+    content: 'CapsLock', code: 'CapsLock', className: 'key_dark key_wide', classNameRu: 'key_dark key_wide',
   },
   {
-    content: 'j', code: 'KeyJ', className: 'key_light', contentShift: 'J', contentRu: 'о', contentShiftRu: 'О',
+    content: 'a', code: 'KeyA', className: 'key_light', contentShift: 'A', classNameRu: 'key_light', contentRu: 'ф', contentShiftRu: 'Ф',
   },
   {
-    content: 'k', code: 'KeyK', className: 'key_light', contentShift: 'K', contentRu: 'л', contentShiftRu: 'Л',
+    content: 's', code: 'KeyS', className: 'key_light', contentShift: 'S', classNameRu: 'key_light', contentRu: 'ы', contentShiftRu: 'Ы',
   },
   {
-    content: 'l', code: 'KeyL', className: 'key_light', contentShift: 'L', contentRu: 'д', contentShiftRu: 'Д',
+    content: 'd', code: 'KeyD', className: 'key_light', contentShift: 'D', classNameRu: 'key_light', contentRu: 'в', contentShiftRu: 'В',
   },
   {
-    content: ';', code: 'Semicolon', className: 'key_light key_twin', contentShift: ':', contentRu: 'ж', contentShiftRu: 'Ж',
+    content: 'f', code: 'KeyF', className: 'key_light', contentShift: 'F', classNameRu: 'key_light', contentRu: 'а', contentShiftRu: 'А',
   },
   {
-    content: '\'', code: 'Quote', className: 'key_light key_twin', contentShift: '"', contentRu: 'э', contentShiftRu: 'Э',
-  },
-  { content: 'ENTER', code: 'Enter', className: 'key_dark key_wide' },
-  {
-    content: 'Shift', code: 'ShiftLeft', className: 'key_dark key_wide', state: false,
+    content: 'g', code: 'KeyG', className: 'key_light', contentShift: 'G', classNameRu: 'key_light', contentRu: 'п', contentShiftRu: 'П',
   },
   {
-    content: '\\', code: 'IntlBackslash', className: 'key_light key_twin', contentShift: '|', contentRu: '\\', contentShiftRu: '/',
+    content: 'h', code: 'KeyH', className: 'key_light', contentShift: 'H', classNameRu: 'key_light', contentRu: 'р', contentShiftRu: 'Р',
   },
   {
-    content: 'z', code: 'KeyZ', className: 'key_light', contentShift: 'Z', contentRu: 'я', contentShiftRu: 'Я',
+    content: 'j', code: 'KeyJ', className: 'key_light', contentShift: 'J', classNameRu: 'key_light', contentRu: 'о', contentShiftRu: 'О',
   },
   {
-    content: 'x', code: 'KeyX', className: 'key_light', contentShift: 'X', contentRu: 'ч', contentShiftRu: 'Ч',
+    content: 'k', code: 'KeyK', className: 'key_light', contentShift: 'K', classNameRu: 'key_light', contentRu: 'л', contentShiftRu: 'Л',
   },
   {
-    content: 'c', code: 'KeyC', className: 'key_light', contentShift: 'C', contentRu: 'с', contentShiftRu: 'С',
+    content: 'l', code: 'KeyL', className: 'key_light', contentShift: 'L', classNameRu: 'key_light', contentRu: 'д', contentShiftRu: 'Д',
   },
   {
-    content: 'v', code: 'KeyV', className: 'key_light', contentShift: 'V', contentRu: 'м', contentShiftRu: 'М',
+    content: ';', code: 'Semicolon', className: 'key_light key_twin', contentShift: ':', classNameRu: 'key_light', contentRu: 'ж', contentShiftRu: 'Ж',
   },
   {
-    content: 'b', code: 'KeyB', className: 'key_light', contentShift: 'B', contentRu: 'и', contentShiftRu: 'И',
+    content: '\'', code: 'Quote', className: 'key_light key_twin', contentShift: '"', classNameRu: 'key_light', contentRu: 'э', contentShiftRu: 'Э',
   },
   {
-    content: 'n', code: 'KeyN', className: 'key_light', contentShift: 'N', contentRu: 'т', contentShiftRu: 'Т',
+    content: 'ENTER', code: 'Enter', className: 'key_dark key_wide', classNameRu: 'key_dark key_wide',
   },
   {
-    content: 'm', code: 'KeyM', className: 'key_light', contentShift: 'M', contentRu: 'ь', contentShiftRu: 'Ь',
+    content: 'Shift', code: 'ShiftLeft', className: 'key_dark key_wide', state: false, classNameRu: 'key_dark key_wide',
   },
   {
-    content: ',', code: 'Comma', className: 'key_light key_twin', contentShift: '<', contentRu: 'б', contentShiftRu: 'Б',
+    content: '\\', code: 'IntlBackslash', className: 'key_light key_twin', contentShift: '|', classNameRu: 'key_light key_twin', contentRu: '\\', contentShiftRu: '/',
   },
   {
-    content: '.', code: 'Period', className: 'key_light key_twin', contentShift: '>', contentRu: 'ю', contentShiftRu: 'Ю',
+    content: 'z', code: 'KeyZ', className: 'key_light', contentShift: 'Z', classNameRu: 'key_light', contentRu: 'я', contentShiftRu: 'Я',
   },
   {
-    content: '/', code: 'Slash', className: 'key_light key_twin', contentShift: '?', contentRu: '.', contentShiftRu: ',',
+    content: 'x', code: 'KeyX', className: 'key_light', contentShift: 'X', classNameRu: 'key_light', contentRu: 'ч', contentShiftRu: 'Ч',
   },
-  { content: '&uarr;', code: 'ArrowUp', className: 'key_dark' },
   {
-    content: 'eng', code: 'LanguageSwitch', className: 'key_dark', lang: false, contentRu: 'рус',
+    content: 'c', code: 'KeyC', className: 'key_light', contentShift: 'C', classNameRu: 'key_light', contentRu: 'с', contentShiftRu: 'С',
   },
-  { content: 'Ctrl', code: 'ControlLeft', className: 'key_dark' },
-  { content: 'Win', code: 'MetaLeft', className: 'key_dark' },
-  { content: 'Alt', code: 'AltLeft', className: 'key_dark' },
-  { content: ' ', code: 'Space', className: 'key_light key_widest' },
-  { content: 'Alt', code: 'AltRight', className: 'key_dark' },
-  { content: 'Ctrl', code: 'ControlRight', className: 'key_dark' },
-  { content: '&larr;', code: 'ArrowLeft', className: 'key_dark' },
-  { content: '&darr;', code: 'ArrowDown', className: 'key_dark' },
-  { content: '&rarr;', code: 'ArrowRight', className: 'key_dark' },
+  {
+    content: 'v', code: 'KeyV', className: 'key_light', contentShift: 'V', classNameRu: 'key_light', contentRu: 'м', contentShiftRu: 'М',
+  },
+  {
+    content: 'b', code: 'KeyB', className: 'key_light', contentShift: 'B', classNameRu: 'key_light', contentRu: 'и', contentShiftRu: 'И',
+  },
+  {
+    content: 'n', code: 'KeyN', className: 'key_light', contentShift: 'N', classNameRu: 'key_light', contentRu: 'т', contentShiftRu: 'Т',
+  },
+  {
+    content: 'm', code: 'KeyM', className: 'key_light', contentShift: 'M', classNameRu: 'key_light', contentRu: 'ь', contentShiftRu: 'Ь',
+  },
+  {
+    content: ',', code: 'Comma', className: 'key_light key_twin', contentShift: '<', classNameRu: 'key_light', contentRu: 'б', contentShiftRu: 'Б',
+  },
+  {
+    content: '.', code: 'Period', className: 'key_light key_twin', contentShift: '>', classNameRu: 'key_light', contentRu: 'ю', contentShiftRu: 'Ю',
+  },
+  {
+    content: '/', code: 'Slash', className: 'key_light key_twin', contentShift: '?', classNameRu: 'key_light key_twin', contentRu: '.', contentShiftRu: ',',
+  },
+  {
+    content: '&uarr;', code: 'ArrowUp', className: 'key_dark', classNameRu: 'key_dark',
+  },
+  {
+    content: 'eng', code: 'LanguageSwitch', className: 'key_dark', lang: false, classNameRu: 'key_dark', contentRu: 'рус',
+  },
+  {
+    content: 'Ctrl', code: 'ControlLeft', className: 'key_dark', classNameRu: 'key_dark',
+  },
+  {
+    content: 'Win', code: 'MetaLeft', className: 'key_dark', classNameRu: 'key_dark',
+  },
+  {
+    content: 'Alt', code: 'AltLeft', className: 'key_dark', classNameRu: 'key_dark',
+  },
+  {
+    content: ' ', code: 'Space', className: 'key_light key_widest', classNameRu: 'key_light key_widest', contentRu: ' ',
+  },
+  {
+    content: 'Alt', code: 'AltRight', className: 'key_dark', classNameRu: 'key_dark',
+  },
+  {
+    content: 'Ctrl', code: 'ControlRight', className: 'key_dark', classNameRu: 'key_dark',
+  },
+  {
+    content: '&larr;', code: 'ArrowLeft', className: 'key_dark', classNameRu: 'key_dark',
+  },
+  {
+    content: '&darr;', code: 'ArrowDown', className: 'key_dark', classNameRu: 'key_dark',
+  },
+  {
+    content: '&rarr;', code: 'ArrowRight', className: 'key_dark', classNameRu: 'key_dark',
+  },
 ];
 
 const textarea = document.querySelector('.main__textarea');
@@ -275,6 +305,8 @@ function createKeyboard(language) {
   keysAll.forEach((keyObj) => {
     const temp = keyObj;
     const className = `key ${keyObj.className}`;
+    const classNameRu = `key ${keyObj.classNameRu}`;
+
     if (language === 'eng') {
       const button = createElement('li', className, keyObj.content, keyboardList);
       temp.element = button;
@@ -283,11 +315,11 @@ function createKeyboard(language) {
         createElement('span', 'key__addition', keyObj.contentShift, button);
       }
     } else {
-      const button = createElement('li', className, keyObj.contentRu, keyboardList);
+      const button = createElement('li', classNameRu, keyObj.contentRu || keyObj.content, keyboardList);
       temp.element = button;
 
       if (button.classList.contains('key_twin')) {
-        createElement('span', 'key__addition', keyObj.contentShiftRu, button);
+        createElement('span', 'key__addition', keyObj.contentShiftRu || keyObj.contentShift, button);
       }
     }
   });
@@ -341,6 +373,8 @@ function actionKey(objArg, source) {
       if ((!getKeyByCode('LanguageSwitch').lang)) {
         if (obj.code.startsWith('Key')) {
           addValueTextarea(textarea, obj.contentShift);
+        } else if (obj.code.startsWith('Shift')) {
+          addValueTextarea(textarea, '');
         } else {
           addValueTextarea(textarea, obj.content);
         }
@@ -354,6 +388,8 @@ function actionKey(objArg, source) {
           || obj.code === 'Comma'
           || obj.code === 'Period')) {
           addValueTextarea(textarea, obj.contentShiftRu);
+        } else if (obj.code.startsWith('Shift')) {
+          addValueTextarea(textarea, '');
         } else {
           addValueTextarea(textarea, obj.contentRu);
         }
