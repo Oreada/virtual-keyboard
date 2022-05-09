e41e06ccb0efe3aa296c9b01a5158dd1bd1f3cc0
Olga Antushevich <antushevich@gmail.com>
2022-05-09 19:39:21 +0300

    feat: add favicon.ico file

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,56 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const conf = {
-	entry: "./src/js/index.js",
+  entry: './src/js/index.js',
 
-	output: {
-		path: path.resolve(__dirname, "./dist"),
-		filename: "[name].[hash].js",
-		clean: true,
-	},
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[hash].js',
+    clean: true,
+  },
 
-	devServer: {
-		static: "./dist",
-	},
+  devServer: {
+    static: './dist',
+  },
 
-	optimization: {
-		runtimeChunk: "single",
-	},
+  optimization: {
+    runtimeChunk: 'single',
+  },
 
-	mode: "development",
+  mode: 'development',
 
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: "Virtual keyboard",
-			filename: "index.html",
-			template: "src/index.html",
-		})
-	],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Virtual keyboard',
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
 
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: "asset/resource",
-			},
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					// Creates `style` nodes from JS strings
-					"style-loader",
-					// Translates CSS into CommonJS
-					"css-loader",
-					// Compiles Sass to CSS
-					"sass-loader",
-				],
-			},
-		]
-	}
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
 };
 
 module.exports = conf;
